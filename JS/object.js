3d2f6460579c1ead78e4b4b56f41fcfe4fe86026
Rifki Andriyan Pratama <rifkiandriyanpratama.com>
2024-09-03 16:50:27 +0700

    Prototyoe

--- JS/object.js
+++ JS/object.js
@@ -1,28 +1,77 @@
-// Object Literal
-var siswa = {
-  nama: "Danu Wisesa",
-  umur: 198,
-  alamat: {
-    desa: "Tanjung Karang",
-    kecamatan: "Jetis",
-  },
-};
+// // Object Literal(Tidak efektif untuk object yang banyak)
+// var siswa = {
+//   nama: "Danu Wisesa",
+//   umur: 198,
+//   alamat: {
+//     desa: "Tanjung Karang",
+//     kecamatan: "Jetis",
+//   },
+// };
 
-// Function Declaration
-function buatObjectSiswa(nama, umur) {
-  var siswa = {};
-  siswa.nama = nama;
-  siswa.umur = umur;
-  return siswa;
+// var siswa2 = {
+//   nama: "Rifki Andriyan Pratama",
+//   umur: 1900,
+//   alamat: {
+//     desa: "Turi",
+//     kecamatan: "Bambanglipuro",
+//   },
+// };
+
+// // Function Declaration
+// const MethodSiswa = {
+//   makan: function (porsi) {
+//     this.energi += porsi;
+//     console.log(`Halo ${this.nama}, Selamat Makan!`);
+//   },
+// };
+
+// function Siswa(nama, energi) {
+//   let siswa = Object.create(MethodSiswa);
+//   siswa.nama = nama;
+//   siswa.energi = energi;
+//   return siswa;
+// }
+
+// let Rifki = Siswa("Rifki", 1000);
+// // Constructor Function
+// // function Siswa() {
+// //   let Siswa = Object.create(buatObjectSiswa);
+// //   return Siswa;
+// // }
+
+// var siswa3 = new Siswa("huhuhu", "19000");
+
+// // Object.create()
+
+// function Siswa(nama, energi) {
+//   this.nama = nama;
+//   this.energi = energi;
+// }
+
+// Siswa.prototype.makan = function (porsi) {
+//   this.energi += porsi;
+//   return `Halo ${this.nama}, selamat makan`;
+// };
+
+// Siswa.prototype.main = function (jam) {
+//   this.energi -= jam;
+//   return `Halo ${this.nama}, Selamat main`;
+// };
+
+// let rifki = new Siswa("Rifki", 100);
+
+//versi class
+
+class Siswa {
+  constructor(nama, energi) {
+    this.nama = nama;
+    this.energi = energi;
+  }
+
+  makan(porsi) {
+    this.energi += porsi;
+    return `Halo ${this.nama}, Selamat Makan`;
+  }
 }
 
-var siswa2 = buatObjectSiswa("Yiho", "1900");
-
-// Constructor Function
-function Siswa(nama, umur) {
-  this.nama = nama;
-  this.umur = umur;
-}
-
-var siswa3 = new Siswa("huhuhu", "19000");
-// Object.create()
+let rifki = new Siswa("Rifki", 1000);
